7ccade668daf941031e1da74872c266af1454754
Sliver_0812 <pwwwle0812@gmail.com>
2024-07-06 16:23:55 +0800

    重置

--- myblog/src/main.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import './style.css'
-import './assets/styles.css'
-import App from './App.vue'
-import router from './router.js';
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
