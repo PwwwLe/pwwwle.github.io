085d5a9f430f7e453acb7e7aab9ebc04dbe2cbae
Sliver_0812 <pwwwle0812@gmail.com>
2024-07-06 16:23:44 +0800

    重置

---
+++ myblog/src/views/About.vue
@@ -0,0 +1,19 @@
+<!-- About.vue -->
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is the about page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ myblog/src/views/Home.vue
@@ -0,0 +1,24 @@
+<!-- Home.vue -->
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Welcome to my blog!</p>
+  </div>
+</template>
+
+<script>
+import blogList from "../components/BlogList.vue";
+
+export default {
+  name: 'Home',
+  components: {
+    blogList,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
